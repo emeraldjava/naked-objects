a80676bef1e9da106c88a221b9c8b9780fc18a3a
emeraldjava <paul.t.oconnell@gmail.com>
2016-02-25 18:23:51 +0000

    Add basic architype.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # naked-objects
 Experiment with apache isis to implement a naked objects solution 
+
+## https://isis.apache.org/
---
+++ src/main/java/emeraldjava/noip/me/App.java
@@ -0,0 +1,13 @@
+package emeraldjava.noip.me;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
